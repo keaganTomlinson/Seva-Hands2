dfd11a5c8ecacd19b378da97b96d7c9d95d8a087
ST10084431 <130999115+ST10084431@users.noreply.github.com>
2023-07-14 01:00:33 +0300

    init

---
+++ Seva hands/Controllers/VolunteerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Seva_hands.Controllers
+{
+    public class VolunteerController : Controller
+    {
+       
+
+        public IActionResult volunteer()
+        {
+            return View();
+        }
+    }
+}
