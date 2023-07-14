4a84a228cb8f22a3043928510de2cbfccb44c208
ST10084431 <130999115+ST10084431@users.noreply.github.com>
2023-07-14 12:44:40 +0300

    events

---
+++ Seva hands/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Seva_hands.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Events()
+        {
+            return View();
+        }
+    }
+}
