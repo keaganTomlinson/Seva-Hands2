81613b81ba639852d9bcc58b85aed06d9290122a
keaganTomlinson <keagantomlinson6@gmail.com>
2023-08-16 08:22:33 +0200

    added paypal

--- Seva hands/Controllers/HomeController.cs
+++ Seva hands/Controllers/HomeController.cs
@@ -4,10 +4,16 @@ using System.Diagnostics;
 
 namespace Seva_hands.Controllers
 {
+    
+       
+  
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
+        private readonly ILogger<HomeController> _logger;
+        private static string PaypalUrl = "https://www.paypal.com/donate/?hosted_button_id=9A7RM3N4NQV3G";
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -15,29 +21,35 @@ namespace Seva_hands.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.PaypalUrl = HomeController.PaypalUrl;
             return View();
         }
         public IActionResult About()
         {
+            ViewBag.PaypalUrl = HomeController.PaypalUrl;
             return View();
         }
         public IActionResult Donate()
         {
+            ViewBag.PaypalUrl = HomeController.PaypalUrl;
             return View();
         }
      
         public IActionResult Explore()
         {
+            ViewBag.PaypalUrl = HomeController.PaypalUrl;
             return View();
         }
         public IActionResult Privacy()
         {
+            ViewBag.PaypalUrl = HomeController.PaypalUrl;
             return View();
         }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            ViewBag.PaypalUrl = HomeController.PaypalUrl;
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
